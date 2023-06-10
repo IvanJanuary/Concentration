3c26d293d19a015e232412c6f8cf7335e9d34855
Ivan <ivan@ks.local>
2023-06-10 16:17:23 +0300

    Create project Concentration

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ivan on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

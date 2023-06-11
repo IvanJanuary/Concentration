9ada3b729031c185694cf13684860f933f224a40
Ivan <ivan@ks.mtm.kiev.ua>
2023-06-11 12:29:16 +0300

    Model - Card static var identifierNumber, static func identifierGenerator() initialize the identifier

--- Concentration/Concentration/Card.swift
+++ Concentration/Concentration/Card.swift
@@ -11,4 +11,15 @@ struct Card {
     var isFaceUp = false
     var isMatched = false
     var identifier: Int
+    
+    static var identifierNumber = 0
+    
+    static func identifierGenerator() -> Int {
+        identifierNumber += 1
+        return identifierNumber
+    }
+    
+    init() {
+        self.identifier = Card.identifierGenerator()
+    }
 }

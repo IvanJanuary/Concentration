635861c9ca574c000312baededc76467fe4533e9
Ivan <ivan@ks.mtm.kiev.ua>
2023-06-11 13:18:28 +0300

    Let's return to the chooseCard function in ConcentrationGame and write the game logic

--- Concentration/Concentration/ConcenterationGame.swift
+++ Concentration/Concentration/ConcenterationGame.swift
@@ -11,16 +11,30 @@ class ConcentrationGame {
     
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        
+        if !cards[index].isMatched {
+            if let matchingIndex = indexOfOneAndOnlyFaceUpCard, matchingIndex != index {
+                if cards[matchingIndex].identifier == cards[index].identifier {
+                    cards[index].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipDown in cards.indices {
+                    cards[flipDown].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
     }
     
     init(numberOfPairsOfCards: Int) {
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
-            // cards.append(card)
-            // cards.append(card)
-            // or
             cards += [card, card]
         }
     }

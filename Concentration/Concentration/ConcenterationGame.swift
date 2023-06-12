4ddd57c99d0a15fbd6af32c4f85c5a22bd9cd0c7
Ivan <ivan@ks.local>
2023-06-12 12:58:22 +0300

    In the ViewController: Update our View - updateViewFromModel() Create func updateViewFromModel() нужно знать перевернута ли карточка Remove old func flipButton Insert more emoji into the emojiCollection Array Create emojiDictionary

--- Concentration/Concentration/ConcenterationGame.swift
+++ Concentration/Concentration/ConcenterationGame.swift
@@ -37,5 +37,6 @@ class ConcentrationGame {
             let card = Card()
             cards += [card, card]
         }
+        // TODO: - Shuffle the cards
     }
 }
--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -16,18 +16,50 @@ class ViewController: UIViewController {
             touchLabel.text = "Touches: \(touches)"
         }
     }
+
+//    Эту старую функцию теперь можно удалить
+//    func flipButton(emoji: String, button: UIButton) {
+//        if button.currentTitle == emoji {
+//            button.setTitle("", for: .normal)
+//            button.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+//        } else {
+//            button.setTitle(emoji, for: .normal)
+//            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+//        }
+//    }
     
-    func flipButton(emoji: String, button: UIButton) {
-        if button.currentTitle == emoji {
-            button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    var emojiCollection = ["🐘", "🦓", "🐅", "🐪", "🦛", "🐃", "🐊", "🦒", "🦩", "🦨", "🐈‍⬛", "🦧"]
+    
+    var emojiDictionary = [Int: String]()
+    
+    func emojiIdentifier(for card: Card) -> String {
+        // let chosenEmoji = emojiDictionary[card.identifier]
+        // Еще один способ как безопасно разворачивать опциональный тип
+        // if emojiDictionary[card.identifier] != nil {
+        //    return emojiDictionary[card.identifier]!
+        // } else {
+        //    return "?"
+        // }   это все удаляем lines 36-42
+        if emojiDictionary[card.identifier] == nil {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiCollection.count)))
+            emojiDictionary[card.identifier] = emojiCollection.remove(at: randomIndex)
         }
+        return emojiDictionary[card.identifier] ?? "?"
     }
     
-    let emojiCollection = ["🦊", "🐰", "🦊", "🐰"]
+    func updateViewFromModel() {
+        for index in buttonCollection.indices {
+            let button = buttonCollection[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emojiIdentifier(for: card), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 0) : #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+            }
+        }
+    }
     
     @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet weak var touchLabel: UILabel!
@@ -35,6 +67,7 @@ class ViewController: UIViewController {
         touches += 1
         guard let buttonIndex = buttonCollection.firstIndex(of: sender) else { return }
         game.chooseCard(at: buttonIndex)
+        updateViewFromModel()
         
     }
     

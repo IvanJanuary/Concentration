cd9819176f20a4b4c8607edfb2a42d04b988726f
Ivan <ivan@ks.mtm.kiev.ua>
2023-06-15 13:00:19 +0300

    Access Control in Card, ConcentrationGame and ViewController

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -9,23 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = ConcentrationGame(numberOfPairsOfCards: numberOfPairsOfCards)
+    private lazy var game = ConcentrationGame(numberOfPairsOfCards: numberOfPairsOfCards)
     
     var numberOfPairsOfCards: Int {
         return (buttonCollection.count + 1) / 2
     }
     
-    var touches = 0 {
+    private (set) var touches = 0 {
         didSet {
             touchLabel.text = "Touches: \(touches)"
         }
     }
     
-    var emojiCollection = ["🐘", "🦓", "🐅", "🐪", "🦛", "🐃", "🐊", "🦒", "🦩", "🦨", "🐈‍⬛", "🦧"]
+    private var emojiCollection = ["🐘", "🦓", "🐅", "🐪", "🦛", "🐃", "🐊", "🦒", "🦩", "🦨", "🐈‍⬛", "🦧"]
     
-    var emojiDictionary = [Int: String]()
+    private var emojiDictionary = [Int: String]()
     
-    func emojiIdentifier(for card: Card) -> String {
+    private func emojiIdentifier(for card: Card) -> String {
 
         if emojiDictionary[card.identifier] == nil {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiCollection.count)))
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
         return emojiDictionary[card.identifier] ?? "?"
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in buttonCollection.indices {
             let button = buttonCollection[index]
             let card = game.cards[index]
@@ -48,9 +48,9 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBOutlet var buttonCollection: [UIButton]!
-    @IBOutlet weak var touchLabel: UILabel!
-    @IBAction func buttonAction(_ sender: UIButton) {
+    @IBOutlet private var buttonCollection: [UIButton]!
+    @IBOutlet private weak var touchLabel: UILabel!
+    @IBAction private func buttonAction(_ sender: UIButton) {
         touches += 1
         guard let buttonIndex = buttonCollection.firstIndex(of: sender) else { return }
         game.chooseCard(at: buttonIndex)
